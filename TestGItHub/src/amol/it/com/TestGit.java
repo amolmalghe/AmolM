8ac557d37aab840eb59c3897257acb160a1d95fd
Amol <Amol@Amol-PC>
2018-07-17 00:18:23 +0530

    Error Amol

--- TestGItHub/src/amol/it/com/TestGit.java
+++ TestGItHub/src/amol/it/com/TestGit.java
@@ -1,8 +1,14 @@
 package amol.it.com;
 
 public class TestGit {
+	void add(int a,int b) {
+		int i =a+b;
+
 	public static void main(String[] args) {
 		System.out.println("AMOL");
+		
+			
+		}
 	}
 
 }
